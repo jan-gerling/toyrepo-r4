4bab290609c8d60e96ad2fa094793edc2cba023a
Mauricio Aniche <mauricioaniche@gmail.com>
2020-02-08 10:56:46 +0100

    first commit

---
+++ a/Animal.java
@@ -0,0 +1,26 @@
+package a;
+
+class Animal {
+
+	public static class Dog {
+
+		public void bark() {
+			int a = 1;
+			int b = 2;
+			int c = 3;
+
+			System.out.println(a);
+			System.out.println(b);
+			System.out.println(c);
+
+		}
+	}
+
+	public static class Cat {
+
+		public void scratch() {
+			int x = 1;
+		}
+	}
+}
+
