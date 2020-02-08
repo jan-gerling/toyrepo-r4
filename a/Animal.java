dd9aa00b03c9456c69c5e6566040fb994d7c9d98
Mauricio Aniche <mauricioaniche@gmail.com>
2020-02-08 10:57:28 +0100

    extract method

--- a/Animal.java
+++ a/Animal.java
@@ -4,15 +4,19 @@ class Animal {
 
 	public static class Dog {
 
+		private void print(int a, int b, int c) {
+                        System.out.println(a);
+                        System.out.println(b);
+                        System.out.println(c);
+		}
+
+
 		public void bark() {
 			int a = 1;
 			int b = 2;
 			int c = 3;
 
-			System.out.println(a);
-			System.out.println(b);
-			System.out.println(c);
-
+			print(a, b, c);
 		}
 	}
 
