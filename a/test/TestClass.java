09661d5fbf64aa53d097273f611fd47b9d4974ef
rafaeldurelli <rafael.durelli@ufla.br>
2020-02-10 09:58:52 -0300

    add a test class

---
+++ a/test/TestClass.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+class TestClass {
+
+    @Test
+    void addition() {
+        assertEquals(3, 3);
+    }
+
+}
